e328cfe41411b334f3c92b8e2aa7ebd5f46be853
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-03-01 22:55:49 -0500

    better commenting

--- src/evilballController.js
+++ src/evilballController.js
@@ -1,6 +1,7 @@
 var evilBallController = function(sprite) {
 
   var update = function() {
+    //all of the below code in this function make the edges of the screen wrap, so that if it goes off the left it will come in the right
     if (sprite.x > 800) {
       sprite.x = 0;
     }
@@ -14,7 +15,7 @@ var evilBallController = function(sprite) {
       sprite.y = 480;
     }
   }
-  var changeDirection = function() {
+  var changeDirection = function() { //give the ball its random behaviour that makes the game challenging
     var xVelocity = Math.random() * 300;
     if (xVelocity > 150) {
       xVelocity -= 301;
@@ -23,7 +24,7 @@ var evilBallController = function(sprite) {
     if (yVelocity > 150) {
       yVelocity -= 301;
     }
-    ;
+    
 
     sprite.body.velocity.x = xVelocity;
     sprite.body.velocity.y = yVelocity;
